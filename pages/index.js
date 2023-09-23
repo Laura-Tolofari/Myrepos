80e6d283e9d48aaa615bb402b4f1bfece319a3a4
Laura-Tolofari <erelaura1811@gmail.com>
2023-09-23 17:14:40 +0100

    done

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,21 @@ import Card from "./list "
 import Movies from "./effects"
 import Form from "./form"
 import Products from "./products"
+import Feedbacks from "./components/feedbacks"
 
 export default function Home() {
   return (
     <>
-     <h1>My React</h1> 
+     {/* <h1>My React</h1> 
       <Todo todo='learn usestate'/>
       <Todo todo='learn usestate'/>
       <Todo todo='learn usestate'/>
       <Card/>
       <Movies/>
       <Form/>
-      <Products/> 
+      <Products/>  */}
+      <Feedbacks/>
+
     </>
   )
 }
